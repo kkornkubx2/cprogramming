bee4a48f045d849ae79fd09a5bf4abb8f78605d0
kkornkubx2 <kkornzaza2@gmail.com>
2021-01-12 15:25:29 +0700

    first commit

---
+++ Addition.c
@@ -0,0 +1,23 @@
+#include <stdio.h> //ดึง Lib Input output
+
+void main (){ 
+//ส่วนที่ 2 ฟั่งชั่นหลักของภาษา C คือ Fx Main
+
+/* Start Addition */
+    int x,y,total; //ตัวแปร X , Y , Total
+    
+    printf("Value of x is :");
+    scanf("%d",&x);
+
+    printf("Value of y is :");
+    scanf("%d",&y);
+
+    total = x + y;
+    printf("output is %d\n", total);
+
+
+/* End Addition */
+
+    
+
+}
---
+++ HelloWorld.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main() {
+
+   printf("Thanabadee Thapha \n" ); //ธนบดี  ท่าผา
+   printf("Korn\n"); //กร
+   printf("18\n"); //18
+   printf("Play football\n"); //เล่นบอล
+   printf("Write get Goland\n"); //เล่นภาษา Goland ได้
+   printf("Give money to parents\n"); //ให้ตังพ่อแม่
+
+}
